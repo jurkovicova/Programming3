ab5a81635177d80905252a37db9fdf186afca53d
Michaela <81041361+jurkovicova@users.noreply.github.com>
2021-10-28 13:24:18 +0100

    Update Circle.java

--- CircleDraw/CircleDraw/src/main/java/Shapes/Circle.java
+++ CircleDraw/CircleDraw/src/main/java/Shapes/Circle.java
@@ -2,16 +2,16 @@ package Shapes;
 
 import java.awt.*;
 public class Circle extends Shape {
-    private int rad; // Fields
+    private int r; // Fields
 
     public Circle(Point initPos, Color col, int radius){
         super(initPos, col); // The constructor
-        rad=radius; // Initialize fields
+        r=radius; // Initialize fields
     }
     @Override
     public void draw(Graphics g) { // A method that draws the object in g
         g.setColor(col);
-        g.fillOval(pos.x,pos.y,rad, rad);
+        g.fillOval(pos.x,pos.y,r, r);
     }
 
 }
